f304d40ffe0910b462eb2a27012f3be6aa252962
sergiogval <sergioval@gmail.com>
2022-03-31 17:11:40 -0600

    Update localStorage js to ES6

--- js/localStorage.js
+++ js/localStorage.js
@@ -2,25 +2,25 @@ const nameI = document.getElementById('name');
 const emails = document.getElementById('email');
 const message = document.getElementById('message');
 
-function saveLocalStore() {
+const saveLocalStore = () => {
   const data = {
     name: nameI.value,
     email: emails.value,
     message: message.value,
   };
   localStorage.setItem('data', JSON.stringify(data));
-}
+};
 nameI.addEventListener('input', saveLocalStore);
 emails.addEventListener('input', saveLocalStore);
 message.addEventListener('input', saveLocalStore);
 
-function getLocalStorage() {
+const getLocalStorage = () => {
   if (localStorage.getItem('data')) {
     const data = JSON.parse(localStorage.getItem('data'));
     nameI.value = data.name;
     emails.value = data.email;
     message.value = data.message;
   }
-}
+};
 
-window.addEventListener('load', getLocalStorage());
+window.onload(getLocalStorage);
